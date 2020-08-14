4e63f13d3c3ec58066188eb89e0bf1a3150ca8f1
AndrewJFleming <aflemi1@gmail.com>
2020-08-13 19:47:33 -0700

    Updates

---
+++ precache-manifest.44c2b2006b6bc3f46e36e8cb12d1358c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b956a4620f442049a0b7f38fb004b4fa",
+    "url": "/portraits-of-iowa/index.html"
+  },
+  {
+    "revision": "8e9e217fe7439113dcc8",
+    "url": "/portraits-of-iowa/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e972a3ff0567a5152ff6",
+    "url": "/portraits-of-iowa/static/css/main.6770d013.chunk.css"
+  },
+  {
+    "revision": "8e9e217fe7439113dcc8",
+    "url": "/portraits-of-iowa/static/js/2.33acf01d.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/portraits-of-iowa/static/js/2.33acf01d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e972a3ff0567a5152ff6",
+    "url": "/portraits-of-iowa/static/js/main.5aac0a2c.chunk.js"
+  },
+  {
+    "revision": "4a9a4da563b7768067c8",
+    "url": "/portraits-of-iowa/static/js/runtime-main.b43a8a41.js"
+  }
+]);
